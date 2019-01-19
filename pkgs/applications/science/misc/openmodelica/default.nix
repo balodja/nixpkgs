228932af9964ca5b087a946dcef6cb77d7475acb
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2019-01-19 14:38:55 +0200

    openmodelica: upgrade to 1.13.0

--- pkgs/applications/science/misc/openmodelica/default.nix
+++ pkgs/applications/science/misc/openmodelica/default.nix
@@ -1,26 +1,36 @@
-{lib, stdenv, fetchgit, fetchsvn, autoconf, automake, libtool, gfortran, clang, cmake, gnumake,
-hwloc, jre, lapack, blas, hdf5, expat, ncurses, readline, qt4, webkitgtk, which,
-lp_solve, omniorb, sqlite, libatomic_ops, pkg-config, file, gettext, flex, bison,
-doxygen, boost, openscenegraph, gnome2, xorg, git, bash, gtk2, makeWrapper }:
+{stdenv, fetchFromGitHub, fetchgit, fetchsvn, autoconf, automake, libtool,
+gfortran, clang, cmake, gnumake, hwloc, jre, openblas, hdf5, expat, ncurses,
+readline, qt4, webkit, which, lp_solve, omniorb, sqlite, libatomic_ops,
+pkgconfig, file, gettext, flex, bison, doxygen, boost, openscenegraph, gnome2,
+pangox_compat, xorg, git, bash, gtk2, makeWrapper }:
 
 let
 
-  fakegit = import ./fakegit.nix { inherit lib stdenv fetchgit fetchsvn bash; };
+  fakegit = import ./fakegit.nix {inherit stdenv fetchgit fetchsvn bash;} ;
 
 in
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "openmodelica";
+  version = "1.13.0";
 
-  src = fetchgit (import ./src-main.nix);
+  src = fetchFromGitHub {
+    owner = "OpenModelica";
+    repo = "OpenModelica";
+    rev = "v${version}";
+    fetchSubmodules = true;
+    sha256 = "00vbhvqm79pndx03g1azdsv12yraxnwa5j4sqy5w35bm448ahzjp";
+  };
 
-  buildInputs = [autoconf cmake automake libtool gfortran clang gnumake
-    hwloc jre lapack blas hdf5 expat ncurses readline qt4 webkitgtk which
-    lp_solve omniorb sqlite libatomic_ops pkg-config file gettext flex bison
-    doxygen boost openscenegraph gnome2.gtkglext xorg.libXmu
-    git gtk2 makeWrapper];
+  buildInputs = [autoconf cmake automake libtool gfortran clang gnumake hwloc
+    jre openblas hdf5 expat ncurses readline qt4 webkit which lp_solve omniorb
+    sqlite libatomic_ops pkgconfig file gettext flex bison doxygen boost
+    openscenegraph gnome2.gtkglext pangox_compat xorg.libXmu git gtk2
+    makeWrapper];
 
   hardeningDisable = [ "format" ];
+
+  enableParallelBuilding = true;
 
   patchPhase = ''
     cp -fv ${fakegit}/bin/checkout-git.sh libraries/checkout-git.sh
@@ -38,16 +48,15 @@ stdenv.mkDerivation {
     for e in $(cd $out/bin && ls); do
       wrapProgram $out/bin/$e \
         --prefix PATH : "${gnumake}/bin" \
-        --prefix LIBRARY_PATH : "${lib.makeLibraryPath [ lapack blas ]}"
+        --prefix LIBRARY_PATH : "${stdenv.lib.makeLibraryPath [ openblas ]}"
     done
   '';
 
-  meta = with lib; {
+  meta = with stdenv.lib; {
     description = "An open-source Modelica-based modeling and simulation environment";
     homepage    = "https://openmodelica.org";
     license     = licenses.gpl3;
     maintainers = with maintainers; [ smironov ];
     platforms   = platforms.linux;
-    broken      = true;
   };
 }
