5960410bc96d416ed0599c1f937deb656d74b396
Vladimir Korolev <balodja@gmail.com>
2021-05-11 23:26:53 +0300

    openmodelica: combined package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7896,6 +7896,7 @@ in
     omparser = callPackage ../applications/science/misc/openmodelica/omparser { };
     omedit = libsForQt5.callPackage ../applications/science/misc/openmodelica/omedit { };
     omlibrary = callPackage ../applications/science/misc/openmodelica/omlibrary { };
+    combined = callPackage ../applications/science/misc/openmodelica/combined { };
   };
 
   qarte = libsForQt5.callPackage ../applications/video/qarte { };
